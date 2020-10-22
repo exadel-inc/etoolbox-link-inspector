2ddf2201522e73c2fb49edb4a3ba8c222ef0df1c
Lizaveta Dzmitrenka <lizaveta.dzmitrenka@hp.com>
2020-10-22 16:38:13 +0300

    [EDMTP-295] Refactoring

--- core/src/main/java/com/exadel/linkchecker/core/servlets/FixLinkServlet.java
+++ core/src/main/java/com/exadel/linkchecker/core/servlets/FixLinkServlet.java
@@ -1,7 +1,9 @@
 package com.exadel.linkchecker.core.servlets;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.request.RequestParameter;
 import org.apache.sling.api.resource.ModifiableValueMap;
 import org.apache.sling.api.resource.PersistenceException;
 import org.apache.sling.api.resource.Resource;
@@ -14,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.Servlet;
+import java.util.Optional;
 
 @Component(service = {Servlet.class}, property = "sling.servlet.paths=" + "/bin/exadel/fix-broken-link")
 @SlingServletResourceTypes(
@@ -21,18 +24,36 @@ import javax.servlet.Servlet;
         methods = HttpConstants.METHOD_POST
 )
 public class FixLinkServlet extends SlingAllMethodsServlet {
-    private static final Logger log = LoggerFactory.getLogger(FixLinkServlet.class);
+    private static final Logger LOG = LoggerFactory.getLogger(FixLinkServlet.class);
+
+    private static final String PATH_PARAM = "path";
+    private static final String CURRENT_ELEMENT_NAME_PARAM = "currentElementName";
+    private static final String NEW_LINK_PARAM = "newLink";
 
     @Override
     protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) {
         try {
             ResourceResolver resourceResolver = request.getResourceResolver();
-            Resource resource = resourceResolver.getResource(request.getRequestParameter("path").getString());
-            ModifiableValueMap map = resource.adaptTo(ModifiableValueMap.class);
-            map.put(request.getRequestParameter("currentElementName").getString(), request.getRequestParameter("newLink").getString());
+            String path = getRequestParamString(request, PATH_PARAM);
+            if (StringUtils.isNotBlank(path)) {
+                Resource resource = resourceResolver.getResource(path);
+                if (resource != null) {
+                    ModifiableValueMap map = resource.adaptTo(ModifiableValueMap.class);
+                    if (map != null) {
+                        map.put(getRequestParamString(request, CURRENT_ELEMENT_NAME_PARAM), getRequestParamString(request, NEW_LINK_PARAM));
+                    }
+                }
+            }
             resourceResolver.commit();
         } catch (PersistenceException e) {
-            log.error(e.getMessage(), e);
+            LOG.error(e.getMessage(), e);
         }
     }
+
+    private String getRequestParamString(SlingHttpServletRequest request, String param) {
+        return Optional.ofNullable(request.getRequestParameter(param))
+                .map(RequestParameter::getString)
+                .orElse(StringUtils.EMPTY);
+    }
+
 }
