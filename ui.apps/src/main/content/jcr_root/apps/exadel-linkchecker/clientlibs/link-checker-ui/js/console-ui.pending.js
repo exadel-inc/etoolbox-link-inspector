b3425dccaec43134bdc6c1849d6111c8ffbc01ce
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2020-11-18 23:28:48 +0300

    ref!: UI code refactoring. Functionality split by sub-files. Jira Ticket: EDMTP-304

---
+++ ui.apps/src/main/content/jcr_root/apps/exadel-linkchecker/clientlibs/link-checker-ui/js/console-ui.pending.js
@@ -0,0 +1,32 @@
+/**
+ * Exadel LinkChecker clientlib.
+ * Post-load utility to check the updates, happened in the report list.
+ */
+(function (window, document, $, Granite) {
+    "use strict";
+
+    var CHECK_URL = '/content/exadel-linkchecker/servlet/pendingGenerationCheck';
+    var UPDATE_MSG = Granite.I18n.get('Some links were updated. Changes will be reflected in the report after data feed regeneration');
+
+    $(document).ready(function () {
+        $.ajax({
+            url: CHECK_URL,
+            async: false, // TODO ??
+            type: 'POST',
+            success: function (data, textStatus, xhr) {
+                if (xhr.status === 200) {
+                    var alertPopup = new Coral.Alert().set({
+                        header: {
+                            innerHTML: 'INFO'
+                        },
+                        content: {
+                            textContent: UPDATE_MSG
+                        }
+                    });
+                    alertPopup.classList.add('elc-coral-alert');
+                    document.body.append(alertPopup);
+                }
+            }
+        });
+    });
+})(window, document, Granite.$, Granite);
