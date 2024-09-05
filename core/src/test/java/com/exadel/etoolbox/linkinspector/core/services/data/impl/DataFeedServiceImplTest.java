e865bfc84fe149e3f2d2cdd591fe3bb856c0a83a
aliakseiTraihel <aliaksei.traihel@mmc.com>
2024-09-05 13:17:23 +0300

    [ELI_60] Cache service changes and refactoring redundant code.

--- core/src/test/java/com/exadel/etoolbox/linkinspector/core/services/data/impl/DataFeedServiceImplTest.java
+++ core/src/test/java/com/exadel/etoolbox/linkinspector/core/services/data/impl/DataFeedServiceImplTest.java
@@ -15,10 +15,7 @@
 package com.exadel.etoolbox.linkinspector.core.services.data.impl;
 
 import com.exadel.etoolbox.linkinspector.api.LinkResolver;
-import com.exadel.etoolbox.linkinspector.core.services.cache.GridResourcesCache;
-import com.exadel.etoolbox.linkinspector.core.services.cache.impl.GridResourcesCacheImpl;
 import com.exadel.etoolbox.linkinspector.core.services.data.ConfigService;
-import com.exadel.etoolbox.linkinspector.core.services.data.GenerationStatsProps;
 import com.exadel.etoolbox.linkinspector.core.services.data.GridResourcesGenerator;
 import com.exadel.etoolbox.linkinspector.core.services.data.models.DataFilter;
 import com.exadel.etoolbox.linkinspector.core.services.data.models.GridResource;
@@ -30,8 +27,6 @@ import com.exadel.etoolbox.linkinspector.core.services.resolvers.ExternalLinkRes
 import com.exadel.etoolbox.linkinspector.core.services.resolvers.InternalLinkResolverImpl;
 import com.exadel.etoolbox.linkinspector.core.services.util.CsvUtil;
 import com.exadel.etoolbox.linkinspector.core.services.util.LinkInspectorResourceUtil;
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 import junitx.util.PrivateAccessor;
@@ -48,11 +43,8 @@ import org.mockito.stubbing.Answer;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -70,12 +62,9 @@ import static org.mockito.Mockito.when;
 class DataFeedServiceImplTest {
     private static final String GRID_RESOURCES_GENERATOR_FIELD = "gridResourcesGenerator";
     private static final String RESOURCE_RESOLVER_FACTORY_FIELD = "resourceResolverFactory";
-    private static final String GRID_RESOURCES_CACHE_FIELD = "gridResourcesCache";
     private static final String REPOSITORY_HELPER_FIELD = "repositoryHelper";
     private static final String LINK_HELPER_FIELD = "linkHelper";
     private static final String CONFIG_FIELD = "configService";
-    private static final String CUSTOM_LINK_FIELD = "customLinkResolver";
-    private static final String EXTERNAL_LINK_CHECKER_FIELD = "externalLinkChecker";
 
     private static final String DATAFEED_PATH = "/content/etoolbox-link-inspector/data/datafeed.json";
     private static final String CSV_REPORT_PATH = "/content/etoolbox-link-inspector/download/report.csv";
@@ -90,7 +79,6 @@ class DataFeedServiceImplTest {
     @BeforeEach
     void setup() throws NoSuchFieldException, IOException, URISyntaxException {
         PrivateAccessor.setField(fixture, REPOSITORY_HELPER_FIELD, getRepositoryHelperFromContext());
-        PrivateAccessor.setField(fixture, GRID_RESOURCES_CACHE_FIELD, getGridResourcesCacheFromContext());
         GridResourcesGeneratorImpl gridResourcesGenerator = getGridResourcesGenerator();
         PrivateAccessor.setField(fixture, GRID_RESOURCES_GENERATOR_FIELD, gridResourcesGenerator);
     }
@@ -216,15 +204,5 @@ class DataFeedServiceImplTest {
         RepositoryHelper repositoryHelper = new RepositoryHelperImpl();
         PrivateAccessor.setField(repositoryHelper, RESOURCE_RESOLVER_FACTORY_FIELD, resourceResolverFactory);
         return repositoryHelper;
-    }
-
-    private GridResourcesCache getGridResourcesCacheFromContext() throws NoSuchFieldException {
-        GridResourcesCache gridResourcesCache = new GridResourcesCacheImpl();
-        Cache<String, CopyOnWriteArrayList<GridResource>> cache = CacheBuilder.newBuilder()
-                .maximumSize(100)
-                .expireAfterWrite(100000, TimeUnit.DAYS)
-                .build();
-        PrivateAccessor.setField(gridResourcesCache, GRID_RESOURCES_CACHE_FIELD, cache);
-        return gridResourcesCache;
     }
 }
