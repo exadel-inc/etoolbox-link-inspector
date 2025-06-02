211c7c8f94acead000e77d27e25cba24f026dda3
Stepan Miakchilo <smiakchilo@exadel.com>
2025-06-02 12:38:42 +0300

    [ELI-115] Improved cross-location configuration handling

--- core/src/main/java/com/exadel/etoolbox/linkinspector/core/services/data/impl/ConfigResourceListenerImpl.java
+++ core/src/main/java/com/exadel/etoolbox/linkinspector/core/services/data/impl/ConfigResourceListenerImpl.java
@@ -3,6 +3,7 @@ package com.exadel.etoolbox.linkinspector.core.services.data.impl;
 import com.day.cq.commons.jcr.JcrConstants;
 import com.exadel.etoolbox.linkinspector.core.services.helpers.RepositoryHelper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ClassUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
@@ -15,7 +16,8 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Collection;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.List;
@@ -24,14 +26,17 @@ import java.util.List;
         service = ResourceChangeListener.class,
         immediate = true,
         property = {
-                ResourceChangeListener.PATHS + "=/conf/etoolbox/link-inspector",
+                ResourceChangeListener.PATHS + "=" + ConfigServiceImpl.CONFIG_PATH,
                 ResourceChangeListener.CHANGES + "=ADDED",
                 ResourceChangeListener.CHANGES + "=CHANGED",
                 ResourceChangeListener.CHANGES + "=REMOVED"
         }
 )
 @Slf4j
-public class ConfigChangeListenerImpl implements ResourceChangeListener {
+public class ConfigResourceListenerImpl implements ResourceChangeListener {
+
+    private static final String INTERNAL_PACKAGE = "com.exadel.etoolbox.linkinspector";
+    private static final String UPDATABLE_CONFIG_TOKEN = "?";
 
     @Reference
     private transient ConfigurationAdmin configurationAdmin;
@@ -57,13 +62,47 @@ public class ConfigChangeListenerImpl implements ResourceChangeListener {
     public void onChange(List<ResourceChange> list) {
         try (ResourceResolver resolver = repositoryHelper.getServiceResourceResolver()) {
             for (ResourceChange change : list) {
+                if (change.getPath().equals(ConfigServiceImpl.CONFIG_PATH)) {
+                    // Skip the root path change
+                    continue;
+                }
+                String pid = StringUtils.substringAfterLast(change.getPath(), "/");
+                if (change.getType() == ResourceChange.ChangeType.REMOVED) {
+                    deleteConfiguration(pid);
+                    continue;
+                }
                 Resource node = resolver.getResource(change.getPath());
                 if (node == null) {
-                    log.error("Failed to get properties for pid {}", StringUtils.substringAfterLast(change.getPath(), "/"));
-                    return;
+                    deleteConfiguration(pid);
+                    continue;
                 }
                 updateConfiguration(node);
             }
+        }
+    }
+
+    private Configuration getConfiguration(String pid) {
+        Configuration result = null;
+        try {
+            result = configurationAdmin.getConfiguration(pid);
+            if (result == null) {
+                log.warn("Configuration for pid {} is not found", pid);
+            }
+        } catch (Exception e) {
+            log.error("Failed to get configuration for pid {}", pid, e);
+        }
+        return result;
+    }
+
+    private void deleteConfiguration(String pid) {
+        Configuration configuration = getConfiguration(pid);
+        if (configuration == null) {
+            return;
+        }
+        try {
+            configuration.delete();
+        } catch (Exception e) {
+            log.error("Failed to delete configuration for pid {}", pid, e);
         }
     }
 
@@ -74,6 +113,11 @@ public class ConfigChangeListenerImpl implements ResourceChangeListener {
         Configuration configuration = getConfiguration(node.getName());
         if (configuration == null) {
             return;
+        }
+        boolean isForeignConfig = !node.getName().startsWith(INTERNAL_PACKAGE);
+        String bundleLocation = configuration.getBundleLocation();
+        if (StringUtils.isNotEmpty(bundleLocation) && isForeignConfig && !bundleLocation.startsWith(UPDATABLE_CONFIG_TOKEN)) {
+            configuration.setBundleLocation(UPDATABLE_CONFIG_TOKEN + bundleLocation);
         }
         updateConfiguration(configuration, node.getValueMap());
     }
@@ -87,21 +131,30 @@ public class ConfigChangeListenerImpl implements ResourceChangeListener {
         }
     }
 
-    private Configuration getConfiguration(String pid) {
-        try {
-            return configurationAdmin.getConfiguration(pid);
-        } catch (IOException e) {
-            log.error("Failed to get configuration for pid {}", pid, e);
-        }
-        return null;
-    }
-
     private static Dictionary<String, ?> toDictionary(ValueMap properties) {
         Dictionary<String, Object> dictionary = new Hashtable<>();
         properties.entrySet()
                 .stream()
-                .filter(entry -> !entry.getKey().contains(":") && entry.getValue() != null)
+                .filter(entry -> !StringUtils.startsWithAny(entry.getKey(), "cq:", "granite:", "jcr:", "sling:"))
+                .filter(entry -> isValidDictionaryEntry(entry.getValue()))
                 .forEach(entry -> dictionary.put(entry.getKey(), entry.getValue()));
         return dictionary;
+    }
+
+    private static boolean isValidDictionaryEntry(Object property) {
+        if (property == null) {
+            return false;
+        }
+        if (property instanceof Array) {
+            return isValidDictionaryType(property.getClass().getComponentType());
+        }
+        if (property instanceof Collection) {
+            return ((Collection<?>) property).stream().allMatch(e -> e != null && isValidDictionaryEntry(e.getClass()));
+        }
+        return isValidDictionaryType(property.getClass());
+    }
+
+    private static boolean isValidDictionaryType(Class<?> type) {
+        return ClassUtils.isPrimitiveOrWrapper(type) || String.class.equals(type);
     }
 }
